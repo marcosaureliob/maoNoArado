e5cfffa8bb2f0bf53243f1b9c060e8a577d2a4ff
Marco Santana <marcos.maio96@gmail.com>
2023-05-08 22:09:32 -0300

    Project Structuring

---
+++ src/styles/default.ts
@@ -0,0 +1,17 @@
+export const defaultTheme = {
+  white: '#fff',
+  '--color-base/white': '#ffffff',
+  '--color-base/gray-6': '#f2f2f2',
+  '--color-base/gray-5': '#e0e0e0',
+  '--color-base/gray-4': '#bdbdbd',
+  '--color-base/gray-3': '#828282',
+  '--color-base/gray-2': '#4f4f4f',
+  '--color-base/gray-1': '#333333',
+  '--color-base/orange': '#fe8824',
+  '--color-base/orange-dark': '#ff5400',
+  '--color-base/orange-light': '#fff8f0',
+  '--color-base/turquoise': '#00a3c4',
+  '--color-base/turquoise dark-2': '#023e8a',
+  '--color-base/turquoise-light': '#caf0f8',
+  '--color-base/background': '#fafafa',
+}
