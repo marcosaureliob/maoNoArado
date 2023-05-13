6cca975a4ca6b37ea0525823caafd9fb0ffbd621
Marco Santana <marcos.maio96@gmail.com>
2023-05-13 20:28:07 -0300

    Removal of non-utility files

--- src/@types/styled.d.ts
+++
@@ -1,8 +0,0 @@
-import { defaultTheme } from '@/styles/default'
-import 'styled-components'
-
-type ThemeType = typeof defaultTheme
-
-declare module 'styled-components' {
-  export interface DefaultTheme extends ThemeType {}
-}
