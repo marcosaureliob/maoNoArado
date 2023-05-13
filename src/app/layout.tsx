0b23a9da7472f1da574256d7adbf8729477075c2
Marco Santana <marcos.maio96@gmail.com>
2023-05-13 20:29:03 -0300

    Font configuration and global styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,15 @@
-
-
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 import StyledComponentsRegistry from '../lib/registry'
-const inter = Inter({ subsets: ['latin'] })
+
+import global from '../styles/global.module.scss'
+
+// const inter = Inter({ subsets: ['latin'] })
+const roboto = Roboto({
+  weight: '400',
+  style: 'normal',
+  preload: false,
+})
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,10 +22,11 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="pt-br">
-      <body className={inter.className}>
-     <StyledComponentsRegistry> {children}</StyledComponentsRegistry>
-     </body>
+    <html lang="pt-br" className={global.html}>
+      <body className={roboto.className}>
+        {/* <body className={inter.className}> */}
+        <StyledComponentsRegistry> {children}</StyledComponentsRegistry>
+      </body>
     </html>
   )
 }
