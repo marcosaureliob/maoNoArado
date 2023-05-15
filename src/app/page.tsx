e92560b8f02647986099fc7926fde5772ffb204e
Marco Santana <marcos.maio96@gmail.com>
2023-05-15 13:05:45 -0300

    Configured global styles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
+'use client'
+
+import { globalStyles } from '@/styles/global'
 import { Header } from '@/components/Patterns/Header'
+
 import Home from './home/page'
+
+globalStyles()
 
 export default function App() {
   return (
---
+++ src/styles/global.ts
@@ -0,0 +1,20 @@
+import { globalCss } from '.'
+
+export const globalStyles = globalCss({
+  '*': {
+    margin: 0,
+    padding: 0,
+    border: 'none',
+  },
+
+  body: {
+    backgroundColor: '$background',
+    color: '$gray900',
+    '-webkit-font-smoothing': 'antialiased',
+  },
+
+  'body, input, textarea, button': {
+    fontFamily: 'Roboto',
+    fontWeight: 400,
+  },
+})
