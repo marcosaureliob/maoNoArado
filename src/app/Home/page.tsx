9df0a358a493ac89da5dbb7878f1953ae7384e25
Marco Santana <marcos.maio96@gmail.com>
2023-05-15 14:53:14 -0300

    Examples of styles with stitches

--- src/app/Home/page.tsx
+++ src/app/Home/page.tsx
@@ -1,7 +1,9 @@
+import { HomeContainer } from '@/styles/page/home'
+
 export default function Home() {
   return (
-    <>
+    <HomeContainer>
       <h2>Página Home</h2>
-    </>
+    </HomeContainer>
   )
 }
