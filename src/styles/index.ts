618a71cecca731617ac720093eba3d20cafe9d90
Marco Santana <marcos.maio96@gmail.com>
2023-05-15 12:54:14 -0300

    Installation of points and initial settings

---
+++ src/styles/index.ts
@@ -0,0 +1,23 @@
+import { createStitches } from '@stitches/react'
+
+export const { styled, globalCss, getCssText } = createStitches({
+  theme: {
+    colors: {
+      white: '#ffffff',
+      background: '#fafafa',
+
+      gray100: '#f2f2f2',
+      gray200: '#e0e0e0',
+      gray600: '#bdbdbd',
+      gray700: '#828282',
+      gray800: '#4f4f4f',
+      gray900: '#333333',
+
+      orange500: '#fe8824',
+      orange700: '#ff5400',
+
+      turquoise500: '#00a3c4',
+      turquoise700: '#0077b6',
+    },
+  },
+})
