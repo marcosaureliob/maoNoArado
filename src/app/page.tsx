d744df6a57e3653c8ec759e5aa9d364d3151b59a
João Victor <97269781+joao-leal12@users.noreply.github.com>
2023-05-21 14:20:12 -0300

    com

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,6 @@
 'use client'
 
-import { globalStyles } from '@/styles/global'
-
 import { HomeContainer } from '@/styles/page/home'
-
-globalStyles()
 
 export default function Home() {
   return <HomeContainer></HomeContainer>
