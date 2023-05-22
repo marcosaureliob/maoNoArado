2f3f04bd800c2c699ada51a02744ce30cb23cfc3
Marco Santana <marcos.maio96@gmail.com>
2023-05-21 22:47:09 -0300

    Separating library component

---
+++ src/app/gallery/components/ImageGallery/page.tsx
@@ -0,0 +1,40 @@
+import Image from 'next/image'
+
+import 'lightgallery/css/lightgallery.css'
+import 'lightgallery/css/lg-zoom.css'
+import 'lightgallery/css/lg-thumbnail.css'
+
+import LightGallery from 'lightgallery/react'
+import lgVideo from 'lightgallery/plugins/video'
+
+import { imageDatabase } from '@/database/index'
+
+import { ImageContainer } from '@/styles/page/gallery'
+
+const invertImageDatabase = imageDatabase.slice(0).reverse()
+
+console.log(imageDatabase)
+console.log(invertImageDatabase)
+
+export default function ImageGallery() {
+  return (
+    <LightGallery
+      elementClassNames={ImageContainer()}
+      plugins={[lgVideo]}
+      mode="lg-fade"
+    >
+      {invertImageDatabase.map((image) => {
+        return (
+          <a key={image.title} className="gallery-item" data-src={image.src}>
+            <Image
+              src={image.src}
+              alt={image.title}
+              style={{ objectFit: 'cover' }}
+              fill
+            />
+          </a>
+        )
+      })}
+    </LightGallery>
+  )
+}
--- src/app/gallery/page.tsx
+++ src/app/gallery/page.tsx
@@ -1,41 +1,14 @@
 'use client'
 
-import Image from 'next/image'
-
-import 'lightgallery/css/lightgallery.css'
-import 'lightgallery/css/lg-zoom.css'
-import 'lightgallery/css/lg-thumbnail.css'
-
-import LightGallery from 'lightgallery/react'
-import lgVideo from 'lightgallery/plugins/video'
-
-import { imageDatabase } from '@/database/index'
-
-import { GalleryContainer, ImageContainer } from '@/styles/page/gallery'
+import { GalleryContainer } from '@/styles/page/gallery'
+import ImageGallery from './components/ImageGallery/page'
 
 export default function Gallery() {
   return (
     <GalleryContainer>
       <h2>Galeria de Imagens</h2>
 
-      <LightGallery
-        elementClassNames={ImageContainer()}
-        plugins={[lgVideo]}
-        mode="lg-fade"
-      >
-        {imageDatabase.map((image) => {
-          return (
-            <a key={image.title} className="gallery-item" data-src={image.src}>
-              <Image
-                src={image.src}
-                alt={image.title}
-                style={{ objectFit: 'cover' }}
-                fill
-              />
-            </a>
-          )
-        })}
-      </LightGallery>
+      <ImageGallery />
     </GalleryContainer>
   )
 }
