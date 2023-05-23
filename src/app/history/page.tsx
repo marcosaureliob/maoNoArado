5ff8254bb145f85456d29b1d2dc82b91b98c387a
gideonalves <gideonam@yahoo.com.br>
2023-05-23 09:18:17 -0300

    correção de bug

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,11 +1,79 @@
 'use client'
+// import { HistoryContainer, HistoryContent, CardsContainer, Card, ContainerPresidente, Wrapper } from "@/styles/page/history";
 
-import { HistoryContainer } from '@/styles/page/history'
+import { Target, Binoculars } from "phosphor-react";
 
-export default function Hand() {
-  return (
-    <HistoryContainer>
-      <p>História Mão do Arado</p>
-    </HistoryContainer>
-  )
-}
+// export default function History() {
+//   return (
+//     <HistoryContainer>
+//       <HistoryContent>
+//         <h2>Associação Mão no Arado</h2>
+
+       
+//               <p>Somos uma organização não governamental (ONG), que surgiu em dezembro de 1998 a partir da iniciativa de uma família, que realizava ações dentro de comunidades no bairro de Itaquera, zona leste de São Paulo em épocas natalinas, com a captação e doações de roupas, calçados, brinquedos e alimentos.</p>
+
+//               <p>Em setembro de 2013, foi criada a Associação Beneficente Mão no Arado, para a ampliação do trabalho social em prol das comunidades da região.</p> 
+
+//               <p>Atualmente os projetos e atividades são financiados por meio de doações, parceiros, instituições públicas e privadas, sendo que grande parte da mão de obra que atua na ONG é formada por voluntários e amigos.</p> 
+//       </HistoryContent>
+
+//       <CardsContainer>
+//         <Card>
+//           <div>
+//             <Target size={40} />
+//             <h3>            
+//                 Missão     
+//             </h3>
+//           </div>
+           
+//             <p>Promover proteção básica, por meio do serviço de convivência e fortalecimento de vínculo as      famílias em situação de média e alta vulnerabilidade e risco social.
+//               Além de promover a preparação para o mundo do trabalho e inclusão produtiva, para o desenvolvimento da autonomia, do protagonismo social e da formação cidadã.
+//             </p>
+//         </Card> 
+
+//         <Card>
+//           <div>
+//             <Target size={40} />
+//             <h3>            
+//                 Visão     
+//             </h3>
+//           </div>
+           
+//             <p>Buscamos ser referência no atendimento social, visando mudar a realidade individual, familiar e consequentemente a comunidade a qual atendemos.
+//             </p>
+//         </Card> 
+
+//         <Card>
+//           <div>
+//             <Binoculars size={40} />
+//             <h3>            
+//                 Valores    
+//             </h3>
+//           </div>
+           
+//             <p>Amor ao próximo, acolhimento, ética, respeito, comprometimento, transparência, solidariedade e valorização.
+//             </p>
+//         </Card> 
+
+//       </CardsContainer>
+
+//       <ContainerPresidente>
+//         <h2>Nossa Presidente</h2>
+
+//         <Wrapper>
+//           <img src="https://images.unsplash.com/photo-1611432579402-7037e3e2c1e4?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=765&q=80" alt="" />
+
+//           <div>
+//             <h3>Roseli Machado Ribeiro</h3>
+//             <p>Presidente da Mão no Arado há mais de 24 anos.</p>
+
+//             <p>Apaixonada e dedicada em ajudar pessoas,</p>
+//             <p>Roseli criou a Associação Mão no Arado há</p>
+//             <p>mais de 24 anos.</p>
+//           </div>
+//         </Wrapper>
+//       </ContainerPresidente>
+
+//     </ HistoryContainer>
+//   )
+// }
