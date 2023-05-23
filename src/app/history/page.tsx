12b68b5f1604ea5977f63b32a7f3f834615eb2d4
gideonalves <gideonam@yahoo.com.br>
2023-05-23 19:08:36 -0300

    add img presidente

--- src/app/history/page.tsx
+++ src/app/history/page.tsx
@@ -1,11 +1,97 @@
 'use client'
+import { HistoryContainer, HistoryContent, CardsContainer, Card, ContainerPresident, Wrapper } from "@/styles/page/history";
 
-import { HistoryContainer } from '@/styles/page/history'
+import { Target, Binoculars } from "phosphor-react";
 
-export default function Hand() {
+import BackgroundCard from "@/assets/background-card.svg";
+import Perfil from "@/assets/Perfil.png";
+import Image from 'next/image';
+
+export default function History() {
   return (
     <HistoryContainer>
-      <p>História Mão do Arado</p>
-    </HistoryContainer>
+      <HistoryContent>
+        <h2>Associação Mão no Arado</h2>
+
+       
+              <p>Somos uma organização não governamental (ONG), que surgiu em dezembro de 1998 a partir da iniciativa de uma família, que realizava ações dentro de comunidades no bairro de Itaquera, zona leste de São Paulo em épocas natalinas, com a captação e doações de roupas, calçados, brinquedos e alimentos.</p>
+
+              <p>Em setembro de 2013, foi criada a Associação Beneficente Mão no Arado, para a ampliação do trabalho social em prol das comunidades da região.</p> 
+
+              <p>Atualmente os projetos e atividades são financiados por meio de doações, parceiros, instituições públicas e privadas, sendo que grande parte da mão de obra que atua na ONG é formada por voluntários e amigos.</p> 
+      </HistoryContent>
+
+      <CardsContainer>
+
+        <Image
+              src={BackgroundCard}
+              alt=""
+              fill
+        />
+
+        <Card>
+          <div>
+            <Target size={40} />
+            <h3>            
+                Missão     
+            </h3>
+          </div>
+           
+            <p>Promover proteção básica, por meio do serviço de convivência e fortalecimento de vínculo as      famílias em situação de média e alta vulnerabilidade e risco social.
+              Além de promover a preparação para o mundo do trabalho e inclusão produtiva, para o desenvolvimento da autonomia, do protagonismo social e da formação cidadã.
+            </p>
+        </Card> 
+
+        <Card>
+          <div>
+            <Target size={40} />
+            <h3>            
+                Visão     
+            </h3>
+          </div>
+           
+            <p>Buscamos ser referência no atendimento social, visando mudar a realidade individual, familiar e consequentemente a comunidade a qual atendemos.
+            </p>
+        </Card> 
+
+        <Card>
+          <div>
+            <Binoculars size={40} />
+            <h3>            
+                Valores    
+            </h3>
+          </div>
+           
+            <p>Amor ao próximo, acolhimento, ética, respeito, comprometimento, transparência, solidariedade e valorização.
+            </p>
+        </Card> 
+
+      </CardsContainer>
+
+      <ContainerPresident>
+        <h2>Nossa Presidente</h2>
+
+  
+
+        <Wrapper>
+
+        <Image
+          src={Perfil}
+          alt=""     
+        />
+   
+
+          <div>
+            <h3>Roseli Machado Ribeiro</h3>
+            <p>Presidente da Mão no Arado há mais de 24 anos.</p>
+
+            <p>Apaixonada e dedicada em ajudar pessoas,</p>
+            <p>Roseli criou a Associação Mão no Arado há</p>
+            <p>mais de 24 anos.</p>
+          </div>
+        </Wrapper>
+      </ContainerPresident>
+
+    </ HistoryContainer>
   )
 }
--- src/styles/page/history.ts
+++ src/styles/page/history.ts
@@ -1,12 +1,124 @@
-'use client'
+import { styled } from ".."
 
-import { styled } from '..'
+export const HistoryContainer = styled('div', { 
 
-export const HistoryContainer = styled('div', {
-  width: '100%',
-  maxWidth: '1216px',
+    width: "100%",
+    maxWidth: "1216px",
+    margin: "0 auto",
+   
+    display: "flex",
+    alignItems: "center",
+    flexDirection: "column",
+    textAlign: "center",
 
-  margin: '0 auto',
+ }) 
 
-  border: '1px solid red',
+
+ export const HistoryContent = styled('section', {
+
+     width: "795px",
+     marginTop: "62px",
+     marginBottom: "92px",
+     
+    h2: {
+         fontWeight: "bold",
+         fontSize: "3rem",
+         marginBottom: "2rem",
+         color: "$gray900",
+    },
+
+    p: {
+         fontWeight: "400",
+         fontSize: "1.25rem",
+         lineHeight: "160%",
+         letterSpacing: "0.04em",
+         color: "$gray-800",
+      }
+ })
+
+ export const CardsContainer = styled('section', {
+      display: "flex",
+      gap: "2rem",
+      flexWrap: "wrap",
+      justifyContent: "center",
+      marginBottom: "16rem",
+      position: "relative",
+})
+
+export const Card = styled('div', {
+   textAlign: "left",
+   alignItems: "flex-end",
+   padding: "2rem",
+   boxSizing: "border-box",
+   maxWidth: "590px",
+   borderRadius: "1rem",
+   background:" rgba(255, 255, 255, 0.5)",
+   paddingBlock: "2rem",
+
+   div: {
+      display: "flex",
+      alignItems: "center",
+   },
+
+   h3: {
+      fontWeight: "700",
+      fontSize: "2rem",
+      margin: "0",
+      marginLeft: "1.25rem",
+      color: "$gray-900",
+   },
+
+   p: {
+      fontWeight: "400",
+      fontSize: "1.125rem",
+      letterSpacing:" 0.06em",
+      color: "$gray-900",
+      lineHeight: "160%",
+   }
+})
+
+export const Container = styled('section', {
+   width: "800px",
+   height: "477px",
+    
+})
+
+export const ContainerPresident = styled('section', {
+   // display: "flex",
+   alignItems: "center", 
+   
+   h2: {
+      fontWeight: "700",
+      fontSize: "2rem",
+      lineHeight: "41.6px",
+      paddingBottom: "6.25rem",
+   },
+})
+
+export const Wrapper = styled('div', {
+  
+   display: "flex",
+   flexWrap: "wrap",
+   gap: "2rem",
+   width: "800px",   
+
+   img: {
+      maxWidth: "318px",
+      height: "477px",
+      objectFit: "cover",
+   },
+
+   div: {
+      textAlign: "left",
+
+   },
+
+   h3: {
+      fontWeight: "700",
+      fontSize: "2.2925rem",
+      color: "$gray900",
+      letterSpacing: "0.04em",
+      lineHeight: "130%",
+   }
+
 })
